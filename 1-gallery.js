1df846fd39385ce856b9d787d30a27c25fc815ca
MykolaHryshkov <154623026+MykolaHryshkov@users.noreply.github.com>
2025-12-01 20:15:52 +0000

    Deploying to gh-pages from @ MykolaHryshkov/goit-js-hw-09@d28793bb44da7682d0d926fd263dde0448da5d2b 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,11 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Gallery");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as i}from"./assets/vendor-CgTBfC_f.js";const e=[{preview:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",original:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",description:"Beautiful tree in nature"},{preview:"https://cdn.pixabay.com/photo/2016/02/19/10/00/aurora-1209963__480.jpg",original:"https://cdn.pixabay.com/photo/2016/02/19/10/00/aurora-1209963_1280.jpg",description:"Northern lights in the sky"},{preview:"https://cdn.pixabay.com/photo/2015/03/26/09/39/flowers-690084__480.jpg",original:"https://cdn.pixabay.com/photo/2015/03/26/09/39/flowers-690084_1280.jpg",description:"Colorful spring flowers"},{preview:"https://cdn.pixabay.com/photo/2016/11/29/03/53/adult-1869232__480.jpg",original:"https://cdn.pixabay.com/photo/2016/11/29/03/53/adult-1869232_1280.jpg",description:"Mountain landscape at sunset"},{preview:"https://cdn.pixabay.com/photo/2014/12/21/23/28/road-575427__480.jpg",original:"https://cdn.pixabay.com/photo/2014/12/21/23/28/road-575427_1280.jpg",description:"Empty road through the forest"},{preview:"https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989__480.jpg",original:"https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg",description:"Calm sea at sunrise"}],r=document.querySelector(".gallery");function n(t){return t.map(({preview:o,original:a,description:p})=>`
+      <li class="gallery-item">
+        <a class="gallery-link" href="${a}">
+          <img
+            class="gallery-image"
+            src="${o}"
+            alt="${p}"
+          />
+        </a>
+      </li>`).join("")}r.innerHTML=n(e);new i(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
