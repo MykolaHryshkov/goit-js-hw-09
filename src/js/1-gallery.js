d28793bb44da7682d0d926fd263dde0448da5d2b
unknown <grishkovnikola@gmail.com>
2025-12-01 22:15:28 +0200

    add alt

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,32 @@
-console.log("Gallery")
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import images from "./images.js";
+
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+galleryList.innerHTML = createGalleryMarkup(images);
+
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',     
+  captionPosition: 'bottom', 
+  captionDelay: 250,        
+});
